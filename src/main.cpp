d97643f0238fc34d3a74c77eb68ee5e78ecd9b13
Arnau25 <81559427+Arnau25@users.noreply.github.com>
2021-06-17 12:26:09 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,65 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+File myFile;
+
+void setup()
+{
+
+  Serial.begin(9600);
+  Serial.print("Iniciando SD ...");
+  SPI.begin(18,19,23,5);
+
+  
+  if (!SD.begin(5)) {
+    Serial.println("No se pudo inicializar");
+    return;
+  }
+  Serial.println("inicializacion exitosa");
+ 
+ //Comrpovamos si hay un fichero en la tarjeta
+ if (SD.exists("/archivo.txt")) {
+    Serial.println("example.txt exists.");
+  } 
+
+  else {
+    Serial.println("archivo.txt doesn't exist.");
+  }
+  // Abrimos el fichero para escribir en el, si existe no habrá problema, si no existe lo creará.
+  myFile = SD.open("/archivo.txt", FILE_WRITE);
+  myFile.close();
+
+  if (SD.exists("/archivo.txt")) {
+
+    
+    Serial.println("archivo.txt exists.");
+  } 
+  else {
+    Serial.println("archivo.txt doesn't exist.");
+  }
+
+  //Escribimos en el fichero
+  myFile=SD.open("/archivo.txt",FILE_WRITE);
+  myFile.println("Hola mundo");
+  myFile.close();
+  //Volvemos a abrir el fichero para mostrar lo que hay dentro
+  myFile=SD.open("/archivo.txt");
+  if (myFile) {
+    Serial.println("archivo.txt:");
+    while (myFile.available()) {
+    	Serial.write(myFile.read());
+    }
+    myFile.close(); //cerramos el archivo
+  }
+  
+  else {
+    Serial.println("Error al abrir el archivo");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  
 }
